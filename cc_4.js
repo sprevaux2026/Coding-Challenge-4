a41410b758a0e9cc4bdb933537c4928e277b207b
sprevaux2026 <stevenprevaux@gmail.com>
2025-02-01 14:57:06 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -2,9 +2,20 @@
 let purchaseAmount = 999; // Purchase Amount
 let finalAmount = purchaseAmount;
 
-// Apply a discount if purchase amount is greater than $100
-if (purchaseAmount > 100) {
+
+if (purchaseAmount > 100) { // Apply a discount if purchase amount is greater than $100
     finalAmount = purchaseAmount * 0.9; // 10% discount applied
 }
 
 console.log(`Final amount after discount: $${finalAmount.toFixed(2)}`);
+
+// Task 2: For Loop - Sales Report
+let sales = [800, 905, 2000, 750, 999]; // Sales data
+let totalSales = 0; 
+
+// Using a for loop to calculate total sales
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+
+console.log(`Total Sales: $${totalSales}`);
